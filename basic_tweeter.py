1ba80d1e99e4ba1667693847dd19998853fe7bf7
siamchow <50926434+siamchow@users.noreply.github.com>
2020-11-10 21:51:58 -0500

    Update basic_tweeter.py
    graph with the dictionary(covid_data)
    Lines 35-47

--- basic_tweeter.py
+++ basic_tweeter.py
@@ -3,6 +3,7 @@ from datetime import date
 import data
 import tweepy
 import time
+import matplotlib.pyplot as plt
 
 
 class Tweeter():
@@ -31,6 +32,20 @@ def main():
     while True:
         # a dict of covid data with "MM/-D/YY" as keys
         covid_data = data.get_data()
+        # get inner keys
+        inner_keys = list(covid_data.values())[0].keys()
+        
+        # x-axis is the outer keys
+        x_axis_values = list(map(str, covid_data.keys()))
+        # loop through inner_keys
+        for x in inner_keys:
+        # create a list of values for inner key
+        y_axis_values = [v[x] for v in covid_data.values()]
+        # plot each inner key
+        plt.plot(x_axis_values, y_axis_values, label=x)
+
+        plt.legend()
+  
         today = date.today().strftime("%-m/%-d/%y")
 
         # check if there is data for today
